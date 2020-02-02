5bdcb3bada95b51b8733c058b4461bb660e0af51
spezi77 <spezi7713@gmx.net>
2020-02-03 00:12:56 +0100

    Revert "marlin: switch to IMapper 2.1"
    This reverts commit 94e7b30686438b48b2242d3e14d6a5b932c98b0a.

--- device-common.mk
+++ device-common.mk
@@ -495,7 +495,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.mapper@2.0-impl-2.1
+    android.hardware.graphics.mapper@2.0-impl
 
 # HW Composer
 PRODUCT_PACKAGES += \
